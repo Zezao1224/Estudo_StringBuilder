263ec066540a5f1b9a6a3377316355ee696ad155
Bruno Luiz <gamer@PCZezao.sa-east-1.ec2-utilities.amazonaws.com>
2021-01-14 01:24:39 -0300

    Primeiro commit

---
+++ Ex_StringBuilder/Entites/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex_StringBuilder.Entites
+{
+    class Comment
+    {
+        public String Text { get; set; }
+        public Comment(String text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Ex_StringBuilder/Entites/Post.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex_StringBuilder.Entites
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public String Title { get; set; }
+        public String Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+        public Post()
+        {
+
+        }
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+        public void AddCommnet(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void RemoveCommnet(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+        public override string ToString()
+        {   
+            //buildar uma string sem concatenar 
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes.ToString());
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+
+            //percorrer a lista
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
